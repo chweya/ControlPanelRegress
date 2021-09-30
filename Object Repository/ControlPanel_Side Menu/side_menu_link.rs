89bca3986387bde8a0a55aff47c9f60fb07430ff
Chweya Charles <chweyaacharles@gmail.com>
2021-09-30 16:03:59 +0300

    Added Common Side Menu TC

---
+++ Object Repository/ControlPanel_Side Menu/side_menu_link.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>side_menu_link</name>
+   <tag></tag>
+   <elementGuidId>3ef0f0e2-8213-49cc-a30e-2b2016df1be1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;root&quot;]//p[text()='${name}']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
